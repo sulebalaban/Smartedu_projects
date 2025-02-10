586f448e78221eed8ce723d84f9f2364c0accabb
SULE BALABAN <sule.balaban27@gmail.com>
2025-02-10 18:17:08 +0100

    fix: Corrected variable name in course_detail function

--- smartedu_con/courses/views.py
+++ smartedu_con/courses/views.py
@@ -3,12 +3,21 @@ from. models import Course
 
 
 def course_list(request):
-    courses=Course.object.all()
+    courses=Course.objects.all().order_by('-date')
     
     context={
         'courses':courses
     }
     
-    
-    
+       
     return render(request,'courses.html',context)
+
+def course_detail(request,category_slug,course_id):
+    course=Course.objects.get(category__slug=category_slug, id =course_id)
+    
+    
+    context={
+        'course':course
+    }
+         
+    return render(request,'course.html',context)
