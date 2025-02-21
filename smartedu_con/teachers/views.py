d3ddf009dcf1e447ca64ab462ab61bbc767ffa5b
SULE BALABAN <sule.balaban27@gmail.com>
2025-02-21 19:13:10 +0100

    Updated :teachers.html template

--- smartedu_con/teachers/views.py
+++ smartedu_con/teachers/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from teachers.models import Teacher 
+
 class  TeacherListView(ListView): 
    model=Teacher
    template_name='teachers.html'
    context_object_name='teachers'
    
+class TeacherDetailView(DetailView):
+    model=Teacher 
+    template_name='teacher.html'
+    context_object_name='teacher'
