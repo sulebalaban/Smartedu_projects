4ea304d385520ff93de0efb140466a7d32c1e71a
SULE BALABAN <sule.balaban27@gmail.com>
2025-02-22 20:59:25 +0100

    Added contact.html

--- smartedu_con/pages/urls.py
+++ smartedu_con/pages/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
-from .import views
-
-
+from pages.views import AboutView, IndexView, ContactView
 
 
 urlpatterns = [
-     path('',views.index, name ="index"),
-     path('about/',views.about, name="about"),
+    path('', IndexView.as_view(), name="index"),
+    path('about/', AboutView.as_view(), name="about"),
+    path('contact/', ContactView.as_view(), name="contact"),
 ]
-
--- smartedu_con/pages/views.py
+++ smartedu_con/pages/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.views.generic.edit import FormView
+from courses.models import Course
+from django.contrib.auth.models import User
+from teachers.models import Teacher
 
+class IndexView(TemplateView):
+    template_name = 'index.html'
 
-def index(request):
-    return render(request,'index.html')
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['courses'] = Course.objects.filter(available=True).order_by('-date')[:2]
+        context['total_course'] = Course.objects.filter(available=True).count()
+        context['total_students'] = User.objects.count()
+        context['total_teachers'] = Teacher.objects.count()
+        return context
 
-def about(request):
-    return render(request,'about.html')
+#def index(request):
+#    return render(request, 'index.html')
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+#def about(request):
+#   return render(request, 'about.html')
+
+class ContactView(FormView):
+    template_name = 'contact.html'
+   
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
