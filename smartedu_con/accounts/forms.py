851881dd861cf744ecc51dedd414b2923991852c
SULE BALABAN <sule.balaban27@gmail.com>
2025-02-24 18:59:11 +0100

    Added login form

---
+++ smartedu_con/accounts/forms.py
@@ -0,0 +1,46 @@
+from django import forms 
+from django.contrib.auth.forms import  UserCreationForm
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Username'
+    }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Password'
+    }))
+    
+    
+    
+class RegisterForm(UserCreationForm):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'First Name'
+    }))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Last Name'
+    }))
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Username'
+    }))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Your Email'
+    }))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Password'
+    }))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control',
+        'placeholder': ' Re-type Password '
+    }))
+    
+    
+    class Meta:
+        model=User
+        fields=['first_name','last_name','email','password1','password2']
---
+++ smartedu_con/accounts/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+
+def user_login(request):
+    return render (request,'login.html')
+
+def user_register(request):
+    pass
+
+def user_logout(request):
+    pass
+
+def user_dashboard(request):
+    pass
