d77adddf31f27ec042682278333c708c0190ae97
SULE BALABAN <sule.balaban27@gmail.com>
2025-03-06 23:18:38 +0100

    Deploy vercel

--- smartedu_con/smartedu/settings.py
+++ smartedu_con/smartedu/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 STATICFILES_DIRS =[os.path.join(BASE_DIR,'smartedu_con/static')]
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'smartedu_con/staticfiles')
+
 
 
 # Media files
