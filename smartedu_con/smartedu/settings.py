d10ebc4eb3cf4d87d23a64636151c781ff1bc384
SULE BALABAN <sule.balaban27@gmail.com>
2025-03-07 21:41:08 +0100

    Deploy with vercel

--- smartedu_con/smartedu/settings.py
+++ smartedu_con/smartedu/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-aji^zanupjsou&yy#umjmqa#h3_ahnzn^p79qdw9tm2ih@w8on'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", "smartedu.vercel.app"]
+ALLOWED_HOSTS = ["*"]
+PORT = os.getenv("PORT", 8000)
 
 
 # Application definition
